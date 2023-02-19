39c2eac2315296dc739887c664351bd32e2bf0bd
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-19 11:12:43 +0100

    create factories

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake() -> words(rand(1,3),true),
+            'cover' => fake() -> imageUrl(200,200,'animals',true),
+            'description' => fake() -> sentences(3,true),
+            'vote' => fake() -> randomFloat(1,1,5),
+            'year' => fake() -> year(now())
+        ];
+    }
+}
