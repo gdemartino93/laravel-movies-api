a0d98e72c983e19f2cadc6a57b6f349bd6824015
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-19 17:45:45 +0100

    installde bootstrap

--- vueFrontend/src/main.js
+++ vueFrontend/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 const app = createApp(App)
 
 app.use(router)
