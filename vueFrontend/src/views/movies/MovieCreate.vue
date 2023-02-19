f58d5267e872f23909b56a6efd9bc37dbc2a0723
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-19 19:48:46 +0100

    create form

--- vueFrontend/src/views/movies/MovieCreate.vue
+++ vueFrontend/src/views/movies/MovieCreate.vue
@@ -1,8 +1,25 @@
 <template>
-    <div>
-        <form action="">
-            <input type="color" name="" id="">
-            <input type="text" placeholder="asdasd">
+    <div class="container">
+        <form action="" class="col-12 col-md-8 col-lg-4 mx-auto py-4">
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+                <input type="text" class="form-control">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Description</span>
+                <textarea  type="text-area" class="form-control" />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Vote</span>
+                <input type="number" class="form-control" min="0" max="5">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Year</span>
+                <input type="number" class="form-control">
+            </div>
+            <button class="btn btn-success">Add </button>
+            
+
         </form>
     </div>
 </template>
